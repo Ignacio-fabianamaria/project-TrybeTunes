5839880b25ae036ce6655cde4b3b4a82b41ed4b8
Siigmaf <faby.tbe@gmail.com>
2022-08-30 16:10:54 -0300

    iniciando criação das rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" componet={ Login } />
+          <Route exact path="/search" componet={ Search } />
+          <Route exact path="/album/:id" componet={ Album } />
+          <Route exact path="/favorites" componet={ Favorites } />
+          <Route exact path="/profile" componet={ Profile } />
+          <Route exact path="/profile/edit" componet={ ProfileEdit } />
+          <Route exact path="/#" componet={ NotFound } />
+        </Switch>
+      </Router>
+    );
   }
 }
 
