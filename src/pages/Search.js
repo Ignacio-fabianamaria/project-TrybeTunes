78354648fe6a5cadd5dc0ca31102a1bbe0bdab1e
Siigmaf <faby.tbe@gmail.com>
2022-08-31 17:53:24 -0300

    criando formulário Pesquisar

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,53 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputSearch: '',
+      isEnterBtnSearch: true,
+    };
+  }
+
+  hendleInputSearch = ({ target }) => { // função para capturar o value do inputSearch e chamar função que valida o botão Pesquisar.
+    const { value } = target; // acessando propriedade value do imput
+    this.setState({ inputSearch: value }, () => this.validateBtnSearch());
+  };
+
+  validateBtnSearch = () => {
+    const { inputSearch } = this.state;
+    const validateinputSearch = 2;
+    if (inputSearch.length >= validateinputSearch) {
+      // habilita btn Pesquisar caso o nome digitado tenha 2 caracteres ou mais.
+      this.setState({ isEnterBtnSearch: false });
+    } else {
+      this.setState({ isEnterBtnSearch: true });
+    }
+  };
+
   render() {
+    const { inputSearch, isEnterBtnSearch } = this.state;
+    const { hendleInputSearch, validateBtnSearch } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <h1>Pesquisar Artista</h1>
+        <input
+          type="search"
+          data-testid="search-artist-input"
+          placeholder="Nome do Artista"
+          value={ inputSearch }
+          onChange={ hendleInputSearch }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ isEnterBtnSearch }
+          onClick={ validateBtnSearch }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
