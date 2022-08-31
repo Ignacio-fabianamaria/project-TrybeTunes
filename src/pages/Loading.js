95065622510051dea59e0208de2f92d285057426
Siigmaf <faby.tbe@gmail.com>
2022-08-30 22:28:17 -0300

    adicionado formulário de entrada e validação do btn Entar

---
+++ src/pages/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+}
+export default Loading;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,64 @@
 import React from 'react';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isEnterBtnDisabled: true,
+      loading: false,
+      userName: '',
+    };
+  }
+
+  hendleInputName = ({ target }) => { // função para capturar o value do input userName e chamar função que valida o botão Entrar.
+    // desconstruindo  event.target
+    const { value } = target; // acessando propriedade value do imput
+    this.setState({ userName: value }, () => this.validateBtnEnter());
+  };
+
+  validateBtnEnter = () => { // função para validar os critérios do botão Entrar
+    const { userName } = this.state;
+    const validateUserName = 3;
+    if (userName.length >= validateUserName) {
+      // habilita btn Entrar caso o userName digitado tenha 3 caracteres ou mais.
+      this.setState({ isEnterBtnDisabled: false });
+    } else {
+      this.setState({ isEnterBtnDisabled: true });
+    }
+  };
+
+  handleClickEnter = () => {
+    const { userName } = this.setState;
+  };
+
   render() {
+    const { userName, loading, isEnterBtnDisabled } = this.state;
+    const { hendleInputName } = this;
     return (
       <div data-testid="page-login">
+        {loading && (<Loading />)}
         <h1>Login</h1>
-        <input type="text" data-testid="login-name-input" placeholder="nome" />
-        <button type="submit" data-testid="login-submit-button">Entrar</button>
+        <input
+          type="text"
+          data-testid="login-name-input"
+          placeholder="nome"
+          value={ userName }
+          onChange={ hendleInputName }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ isEnterBtnDisabled }
+          onClick={ async () => {
+            this.setState({ loading: true });
+            await createUser({ name: userName });
+            this.setState({ loading: false });
+          } }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
